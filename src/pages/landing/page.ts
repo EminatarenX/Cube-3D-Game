01e86ea3fc18288881acb9954170b58da75c87a7
Emiliano Nataren Del Rivero <eminataren2002@gmail.com>
2024-09-21 19:38:02 -0600

    cube dash

--- src/pages/landing/page.ts
+++ src/pages/landing/page.ts
@@ -9,8 +9,8 @@ export const landing = {
 <section class="landing h-screen w-full flex flex-col justify-center items-center absolute gap-10 appear">
   <span class="text-white absolute top-10 left-5 text-xl"> Max Jumps IG: ${record.totalJumps}</span>
    <span class="text-white absolute top-20 left-5 text-xl">Max time: ${record?.time}</span>
-    <h1 class="text-white text-6xl text-center" >Cube Dash</h1>
-    <button class=" relative bg-sky-600  rounded hover:bg-teal-500 text-6xl text-white font-bold  p-6 after:absolute after:-top-5 after:-left-5 after:blur shadow-2xl shadow-gray-500 after:-z-10 after:w-full after:h-full after:bg-rose-600 " id="play-button">
+    <h1 class="text-white text-[6rem] text-center animate-bounce" >Cube Dash</h1>
+    <button class=" relative bg-sky-600  rounded hover:bg-teal-500 text-[5rem] transition-all duration-500 text-white font-bold  p-6 after:absolute after:-top-5 after:-left-5 after:blur shadow-2xl shadow-gray-500 after:-z-10 after:w-full after:h-full after:bg-rose-600 " id="play-button">
       Play 
     </button>
 
